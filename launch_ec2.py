285e40b89567af60a0f18504edb8b54f05c34c50
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-10-04 10:49:51 -0400

    Update launch_ec2.py

--- launch_ec2.py
+++ launch_ec2.py
@@ -2,14 +2,17 @@ from itertools import count
 import boto3
 client = boto3.client('ec2')
 
-client.run_instances(
+resp = client.run_instances(
     # provide AMI id found in console; get from AMI Catalog or from instance launch dialog
     ImageId='ami-026b57f3c383c2eec',
 
     # define instance type, e.g. t2.micro for free tier
-    InstanceType='a1.medium'|'a1.large'|'a1.xlarge',
+    InstanceType='t2.micro',
     
     # define min & max number of instances; both set at 1 for demo
     MinCount=1,
     MaxCount=1
 )
+
+for instance in resp['Instances']:
+    print(instance['InstanceId'])
