a2a64a56520d38432f2f6be51f4475049e89a78a
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-10-04 12:19:09 -0400

    Update describe_instances.py

--- describe_instances.py
+++ describe_instances.py
@@ -1,12 +1,35 @@
 import boto3
 client = boto3.client('ec2')
-resp = client.describe_instances()
 
-# to get values from the Return dict, first loop through the Reservations item, then through the Instances items within that
+
+# # get values from the Return dict by looping through the Reservations item, then through the Instances items within that
+# resp = client.describe_instances()
+# for reservation in resp['Reservations']:
+#     for instance in reservation['Instances']:
+#         instance_type = instance['InstanceType']
+#         instance_id = instance['InstanceId']
+#         instance_az = instance['Placement']['AvailabilityZone']
+
+#         print(f'The {instance_type} instance with Id {instance_id} is hosted in the {instance_az} AZ.')
+
+
+# # get description of a *subset* of EC2 instances by using optional Filters argument of the describe_instances() method
+# resp = client.describe_instances(Filters=[{
+#     'Name': 'instance-state-name',
+#     'Values': ['running']
+#     }])
+
+# for reservation in resp['Reservations']:
+#     for instance in reservation['Instances']:
+#         instance_id = instance['InstanceId']
+#         print(f'Instance Id is {instance_id}')
+
+
+# get description of EC2 instances with a certain tag only
+resp = client.describe_instances(Filters=[{'Name': 'tag:Env', 'Values': ['Dev']}])
+
 for reservation in resp['Reservations']:
     for instance in reservation['Instances']:
-        instance_type = instance['InstanceType']
         instance_id = instance['InstanceId']
-        instance_az = instance['Placement']['AvailabilityZone']
+        print(f'Instance Id is {instance_id}')
 
-        print(f'The {instance_type} instance with Id {instance_id} is hosted in the {instance_az} AZ.')
