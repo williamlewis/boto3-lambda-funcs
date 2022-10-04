c85a22540365f9d39af60673d5b0d8f6bf3a1aff
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-10-04 12:59:06 -0400

    Create ec2_collections.py

---
+++ ec2_collections.py
@@ -0,0 +1,25 @@
+import boto3
+ec2 = boto3.resource('ec2') # define as a RESOURCE object, not a CLIENT object
+
+# # without filtering, loop through all instances and print their attributes
+# for instance in ec2.instances.all():
+#     print(f'Instance Id is {instance.instance_id} and instance type is {instance.instance_type}')
+
+
+# # get instance attributes filtered down to a specific AZ
+# for instance in ec2.instances.filter(Filters=[
+#     {
+#         'Name': 'availability-zone',
+#         'Values': ['us-east-1b']
+#     }
+# ]):
+#     print(f'Instance Id is {instance.instance_id} and instance type is {instance.instance_type}')
+
+
+# get instances in a running state, then stop them using the .stop() method (no loop needed)
+ec2.instances.filter(Filters=[
+    {
+        'Name': 'instance-state-name',
+        'Values': ['running']
+    }
+]).stop()
